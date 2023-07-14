0333d2aa3ba97016738ee9f5a7c967c1256db5f8
richie <richie@gmail.com>
2023-07-14 07:59:39 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n". sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a float: %d bytes(s)\n", sizeof(float));
-	return (0)'
+	return (0);
 }
