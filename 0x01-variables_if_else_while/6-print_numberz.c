33dca6a88233b883cbd78423697618ad697c6509
richie <richie@gmail.com>
2023-07-14 22:57:39 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print numbers in base 10'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
