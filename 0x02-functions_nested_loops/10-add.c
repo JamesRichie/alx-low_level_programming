fe66597b5a9b93c7f47025ff1cc3f415cc4bdaaa
richie <richie@gmail.com>
2023-07-18 16:58:46 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds teo integers.
+ * @x: First number to be checked
+ * @y: Second number to be checked
+ * Return: Integer values
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
