bec15f162097820b5df9a18bfc146b72390ea0a2
richie <richie@gmail.com>
2023-07-19 02:20:30 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,9 +20,7 @@ void print_times_table(int n)
 		_putchar(' ');
 		prod = num * mult;
 		if  (prod <= 99)
-		{
 			_putchar(' ');
-		}
 		if (prod >= 100)
 		{
 			_putchar((prod / 100) + '0');
@@ -31,17 +29,10 @@ void print_times_table(int n)
 		else if (prod <= 99 && prod >= 10)
 		{
 			_putchar((prod / 10) + '0');
-			{
-				_putchar((prod % 10) + '0');
-				{
-					_putchar('\n');
-				}
-			}
+		}
+		_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
 		}
 	}
-
 }
-}
-}
-
-
