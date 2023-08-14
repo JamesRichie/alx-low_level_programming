c78bae23c33663036e9dd882849e4b84bfea6927
richie <richie@gmail.com>
2023-08-14 16:15:54 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file
+ * it was compiled from
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
