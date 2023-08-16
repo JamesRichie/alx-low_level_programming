cdb17c63649253406d26883523ce9c961c49f545
richie <richie@gmail.com>
2023-08-16 04:23:28 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Prints a name
+ * @name: Name to be printed 
+ * @f: Pointer to the function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f== NULL)
+		return;
+	f(name);
+}
