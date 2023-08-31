e3aea88e7f0917943ff448f4a134f9ce21c0524b
JamesRichie <jamescj1347@gmail.com>
2023-08-31 07:49:58 -0700

    advanced tasks

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checs the endianness
+ * 
+ * Return: 1 or 0
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
